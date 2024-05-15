fd451ab0fdd5d41c1b627021396832897eb9c9b7
acesarato <151681625+acesarato@users.noreply.github.com>
2024-05-15 12:13:03 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+<script type="text/javascript"> 
+			
+	jQuery(document).ready(function(){ 
+		jQuery("#gallery").unitegallery(); 
+	}); 
+		
+		</script>
+
 require(['./lightgallery.js'], function() {
     require(["./lg-zoom.js", "./lg-thumbnail.js"], function(){
         lightGallery(document.getElementById('lightgallery'));
