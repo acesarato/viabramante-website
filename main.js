b50720e5ee09596bc9b5286cdf5ca797612b7eee
acesarato <151681625+acesarato@users.noreply.github.com>
2024-06-05 00:35:07 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,5 @@ require(['./lightgallery.js'], function() {
     lightGallery(document.getElementById(‘’lightgallery), {
      plugins: [lgZoom, lgAutoplay, lgComment, lgFullscreen , lgHash, lgPager, lgRotate, lgShare, lgThumbnail, lgVideo] 
 });
-<script>
-    lightGallery(document.getElementById('lightgallery'));
-</script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/lightgallery-js/1.4.0/js/lightgallery.min.js"></script>
+
+
