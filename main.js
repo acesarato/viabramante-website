a4d6500ca246cab4e61fc75a98d1bde735b76147
acesarato <151681625+acesarato@users.noreply.github.com>
2024-06-04 23:04:37 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 require(['./lightgallery.js'], function() {
     require(["./lg-zoom.js", "./lg-thumbnail.js"], function(){
-        lightGallery(document.getElementById('lightgallery'));
-    });
+    lightGallery(document.getElementById(‘’lightgallery), {
+     plugins: [lgZoom, lgAutoplay, lgComment, lgFullscreen , lgHash, lgPager, lgRotate, lgShare, lgThumbnail, lgVideo] 
 });
 <script>
     lightGallery(document.getElementById('lightgallery'));
